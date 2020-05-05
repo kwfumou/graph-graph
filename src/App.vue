c686b50346887bc3228e024e7c75eea13089a9b6
kwfumou <kfum2ouw9@gmail.com>
2020-05-06 01:05:59 +0900

    routerにhomeを追加して、整理

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,17 @@
   <div id="app">
     <myheader></myheader>
     <img alt="Vue logo" src="./assets/logo.png" />
-    <Table />
     <router-view />
     <button class="btn btn-primary">bootstrap</button>
   </div>
 </template>
 
 <script>
-import Table from "./components/Table.vue";
 import myheader from "./components/myheader";
 
 export default {
   name: "App",
   components: {
-    Table,
     myheader
   }
 };
@@ -28,6 +25,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 0px;
 }
 </style>
--- src/router.js
+++ src/router.js
@@ -1,5 +1,6 @@
 import Vue from "vue";
 import Router from "vue-router";
+import HelloWorld from "./components/HelloWorld";
 import test1 from "./components/test1";
 import test2 from "./components/test2";
 import test3 from "./components/test3";
@@ -8,6 +9,11 @@ Vue.use(Router);
 
 export default new Router({
   routes: [
+    {
+      path: "/",
+      name: "HelloWorld",
+      component: HelloWorld,
+    },
     {
       path: "/test1",
       name: "test1",
